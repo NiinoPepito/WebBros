e4a3046aa97d0e4ba81bf4e4dc17b492de97deae
NiinoPepito <clemdrb24120@gmail.com>
2024-03-10 23:59:58 +0100

    ouais

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Accueil from "@/components/Accueil";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
-      <h1 className="text-4xl font-bold">WebBros</h1>
+    <main className="flex min-h-screen flex-col container mx-auto px-12 py-4">
+        <Accueil />
     </main>
   );
 }
---
+++ components/Accueil.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const Accueil = () => {
+    return (
+        <section>
+            <div className="flex flex-col items-center">
+                <p className="text-lg lg:text-xl">
+                    Création de site vitrine
+                </p>
+                <h1 className="mb-4 text-4xl lg:text-4xl font-extrabold">
+                    WebBros
+                </h1>
+            </div>
+        </section>
+
+    )
+}
+
+export default Accueil
