1910443dcb31b95a78af28794e0f842ca9268b6f
NiinoPepito <clemdrb24120@gmail.com>
2024-03-14 23:23:16 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,17 @@ import Image from "next/image";
 import Accueil from "@/components/Accueil";
 import AboutUs from "@/components/AboutUs";
 import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <section id={"top"} className="flex min-h-screen flex-col container mx-auto px-12 py-4">
-            <nav>
-                <div className="container">
-                    <div className="logo">
-                        <a href="#top">Our Logo</a>
-                    </div>
-                    <div id="mainListDiv">
-                        <ul>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contact">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <Accueil/>
-            <div className="flex-1">
+        <section>
+            <Navbar/>
+            <div id={"top"} className="flex min-h-screen flex-col container mx-auto px-12 py-4">
+                <Accueil/>
                 <AboutUs/>
+                <Contact/>
             </div>
-            <Contact/>
         </section>
-);
+    );
 }
